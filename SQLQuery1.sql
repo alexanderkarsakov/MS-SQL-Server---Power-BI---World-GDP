7ee9cdf3f2778bb64df6dcce2da2d08e907aecd9
Aleksandr Karsakov <40732933+alexanderkarsakov@users.noreply.github.com>
2021-11-12 10:03:35 +0100

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,8 @@
 
 
-IF OBJECT_ID('Raw_Data_GDP') IS NOT NULL DROP TABLE Raw_Data_GDP
+IF OBJECT_ID('Data') IS NOT NULL DROP TABLE Data
 
-CREATE TABLE Raw_Data_GDP
+CREATE TABLE Data
 (DEMO_IND NVARCHAR(200),
 Indicator NVARCHAR(200),
 [LOCATION] NVARCHAR(200),
@@ -13,36 +13,31 @@ Country NVARCHAR(200),
 Flags NVARCHAR(200)
 )
 
-
-BULK INSERT Raw_Data_GDP
-FROM 'C:\Users\alexa\datasets\gdp_raw_data.csv'
+BULK INSERT Data
+FROM 'C:\Users\alexa\datasets\data.csv'
 WITH ( FORMAT='CSV');
 
-
-SELECT * FROM Raw_Data_GDP
-
-
+SELECT * FROM Data
 
 CREATE VIEW GDP_Excel_Input AS
 
 SELECT a.*, b.GDP_Per_Capita FROM
 
-	(SELECT Country, [Time] AS Year_No, [Value] AS GDP_Value FROM Raw_Data_GDP
+	(SELECT Country, [Time] AS Year_No, [Value] AS GDP_Value FROM Data
 	WHERE Indicator = 'GDP (current US$)' ) a
 
 	LEFT JOIN
-	(SELECT Country, [Time] AS Year_No, [Value] AS GDP_Per_Capita FROM Raw_Data_GDP
+	(SELECT Country, [Time] AS Year_No, [Value] AS GDP_Per_Capita FROM Data
 	WHERE Indicator = 'GDP per capita (current US$)' ) b
 	ON a.Country = b.Country AND a.Year_No = b.Year_No
 
 SELECT * FROM GDP_Excel_Input
 
-
 CREATE PROCEDURE GDP_Excel_Input_Monthly AS
 
-IF OBJECT_ID('Raw_Data_GDP') IS NOT NULL DROP TABLE Raw_Data_GDP
+IF OBJECT_ID('Data') IS NOT NULL DROP TABLE Data
 
-CREATE TABLE Raw_Data_GDP
+CREATE TABLE Data
 (DEMO_IND NVARCHAR(200),
 Indicator NVARCHAR(200),
 [LOCATION] NVARCHAR(200),
@@ -54,8 +49,8 @@ Flags NVARCHAR(200)
 )
 
 
-BULK INSERT Raw_Data_GDP
-FROM 'C:\Users\alexa\datasets\gdp_raw_data.csv'
+BULK INSERT Data
+FROM 'C:\Users\alexa\datasets\data.csv'
 WITH ( FORMAT='CSV');
 
 
